47fc31a8b887c4e798d165b1aa029bfe41c16294
Sunghun Park <sunghun.josephp@gmail.com>
2022-12-20 14:34:42 -0800

    Initialize pages and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+
+// pages
+import Home from "./pages/Home";
+import Projects from './pages/Projects';
+
+// components
+import Header from "./components/Header";
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
