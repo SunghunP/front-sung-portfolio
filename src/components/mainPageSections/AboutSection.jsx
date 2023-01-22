ce74d38d12e57663c1ca66daf72fb21072a71c1b
Joe Park <sunghun.josephp@gmail.com>
2023-01-22 03:21:31 -0800

    Add small styling touches to About me section

--- src/components/mainPageSections/AboutSection.jsx
+++ src/components/mainPageSections/AboutSection.jsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-import profile from '../../images/profile.jpg';
+import profile from '../../assets/images/profile.jpg';
 
 const StyledAboutSection = styled.div`
   width: 100%;
   padding: 10px auto;
 
-  /* .gradient > div.half-column.flex.col {
-    background: #1c1c1c;
+  .gradient > div.half-column.flex.col {
+    background-color: #1c1c1c;
   }
 
 	.gradient {
@@ -14,11 +14,11 @@ const StyledAboutSection = styled.div`
     min-height: 100%;
 		background-image: linear-gradient(
     rgba(25,73,197,1) 0%, 
-    rgba(28,28,28,1) 100%
+    rgba(28,28,28,0.1) 75%
 		);
     padding: 2%;
     box-sizing: border-box;
-	} */
+	}
 
 	.half-column {
 		width: 50vw;
@@ -29,7 +29,7 @@ const StyledAboutSection = styled.div`
 	}
 
 	.links {
-		color: ${props => props.theme.mainText};
+		color: #1949c5;
 		text-decoration: none;
 	}
 
@@ -58,32 +58,34 @@ const StyledAboutSection = styled.div`
 `
 
 export default function AboutSection({theme}) {
-  return  <StyledAboutSection className='about-section flex' theme={theme} profile={profile}>
-    <div className='gradient flex'>
-      <div className="half-column flex col">
-        <h1>Hi, I'm Sung!</h1>
-        <div>
-          <p>
-            I am a Full Stack Web Developer based in Seattle Washington
-          </p>
-          <p>
-            I love to write 
-            <a className='links' href="https://soundcloud.com/joseph-park-775519631"> <i className="fa-solid fa-music"></i> Music <i className="fa-solid fa-music"></i>
-            </a>,
-          </p>
-          <p>
-            cook delicious meals <i className="fa-solid fa-utensils"></i>,
-          </p>
-          <p>
-            and spend time with my golden retriever!
-          </p>
+  return  (
+    <StyledAboutSection className='about-section flex' theme={theme} profile={profile}>
+      <div className='gradient flex'>
+        <div className="half-column flex col">
+          <h1>Hi, I'm Sung!</h1>
+          <div>
+            <p>
+              I am a Full Stack Web Developer based in Seattle Washington
+            </p>
+            <p>
+              I love to write 
+              <a className='links' href="https://soundcloud.com/joseph-park-775519631"> <i className="fa-solid fa-music"></i> Music <i className="fa-solid fa-music"></i>
+              </a>,
+            </p>
+            <p>
+              cook delicious meals <i className="fa-solid fa-utensils"></i>,
+            </p>
+            <p>
+              and spend time with my golden retriever!
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="half-column">
-        <div className='profile-img' alt="Image of Sung">
+        <div className="half-column">
+          <div className='profile-img' alt="Image of Sung">
+          </div>
         </div>
       </div>
-    </div>
-  </StyledAboutSection>
+    </StyledAboutSection>
+  )
 }
