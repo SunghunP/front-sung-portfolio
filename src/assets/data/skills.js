f71d6d9445fe11754ccac0c2049704ea7a3d23a6
Joe Park <sunghun.josephp@gmail.com>
2023-01-23 00:02:16 -0800

    Create Dummy Data

---
+++ src/assets/data/skills.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    'name': 'HTML',
+    'description': 'this is a description',
+    'skillLevel': 5,
+  },
+  {
+    'name': 'CSS',
+    'description': 'CSS is time consuming',
+    'skillLevel': 4,
+  },
+  {
+    'name': 'JavaScript',
+    'description': 'JS is cool',
+    'skillLevel': 4,
+  },
+];
