5c16e9de9f722c383bdd62f1b2ab37b0b1d425bc
Joe Park <sunghun.josephp@gmail.com>
2023-01-23 15:58:13 -0800

    Change description to desc

--- src/assets/data/skills.js
+++ src/assets/data/skills.js
@@ -1,17 +1,17 @@
 module.exports = [
   {
     'name': 'HTML',
-    'description': 'this is a description',
+    'desc': 'this is a description',
     'skillLevel': 5,
   },
   {
     'name': 'CSS',
-    'description': 'CSS is time consuming',
+    'desc': 'CSS is time consuming',
     'skillLevel': 4,
   },
   {
     'name': 'JavaScript',
-    'description': 'JS is cool',
+    'desc': 'JS is cool',
     'skillLevel': 4,
   },
 ];
