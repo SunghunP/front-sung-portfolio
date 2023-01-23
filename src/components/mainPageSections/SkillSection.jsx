3cdef415f4e533cbb1206bb02f11828c2199bbb1
Joe Park <sunghun.josephp@gmail.com>
2023-01-23 15:59:34 -0800

    Use helper function to map out skill data

--- src/components/mainPageSections/SkillSection.jsx
+++ src/components/mainPageSections/SkillSection.jsx
@@ -1,14 +1,32 @@
 import styled from 'styled-components';
 import Skill from '../../components/Skill';
+import mapDataToComponents from '../../helpers/mapDataToComponents';
 
 const StyledDiv = styled.div`
   background: ${props => props.theme.reverseBackground};
   color: ${props => props.theme.reverseText};
+  align-items: center;
+  justify-content: center;
+
+  .wrapper {
+    max-width: 80vw;
+    border: 5px solid brown;
+    flex-wrap: wrap;
+  }
 `;
 
-export default function SkillSection({theme}) {
-  return <StyledDiv theme={theme}>
-    <h1>Skills</h1>
-    <Skill></Skill>
-  </StyledDiv>
+const Styledh1 = styled.h1`
+  color: ${props => props.theme.mainBackground};
+`;
+
+export default function SkillSection({theme, skillList}) {
+  return <>
+    <StyledDiv theme={theme} className='flex col'>
+      <Styledh1 theme={theme} >SKILLS</Styledh1>
+      <div className='wrapper flex'>
+        {mapDataToComponents(skillList, Skill)}
+      </div>
+    </StyledDiv>
+  </>
+  
 };
