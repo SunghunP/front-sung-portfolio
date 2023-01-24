cfd81fc7579779c6b4111fc13cfc34514f2f7e85
Joe Park <sunghun.josephp@gmail.com>
2023-01-23 18:32:28 -0800

    Complete structure and style for Skill component

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,29 +1,43 @@
 import styled from 'styled-components';
 
 const StyledCard = styled.div`
-  margin: 0px auto;
-  border: solid blue 2px;
-  margin: 2%;
-  overflow-wrap: wrap;
+  color: ${props => props.theme.mainBackground};
+  background: linear-gradient(to left, #12D8FA  75%, #199FC5  120%);
+  padding: 1%;
+  margin: 3% 2%;
+  width: 40%;
+  height: 150px;
+  border-radius: 20px;
+  border: 1px solid #11acd1;
+  box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.4);
+  align-items: center;
+  justify-content: flex-start;
 
   .title {
-
+    color: ${props => props.theme.mainBackground};
   }
 
   .card-content{
+    color: ${props => props.theme.mainDarkMode};
+    justify-content: space-between;
+    height: 100%;
+    margin-bottom: 5%;
+  }
 
+  div {
+    text-align: center;
   }
 `;
 
-export default function Skill({e}) {
+export default function Skill({theme, e}) {
   return (
-    <StyledCard className='card'>
-      <div className='title'>
-        <h1>{e.name}</h1>
-        <div className='card-content'>
-          <div>{e.desc}</div>
-          <div>{e.skillLevel}</div>
-        </div>
+    <StyledCard className='card flex col' theme={theme}>
+      <div className='card-title'>
+        <h3 className='title'>{e.name}</h3>
+      </div>
+      <div className='card-content flex col'>
+        <div>{e.desc}</div>
+        <div>{e.skillLevel}</div>
       </div>
     </StyledCard>
   );
