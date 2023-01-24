3c8bbb5fd86dacc20533b19f6e391b51cfb9172c
Joe Park <sunghun.josephp@gmail.com>
2023-01-23 16:00:14 -0800

    Create card styling for skill component

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,15 +1,30 @@
 import styled from 'styled-components';
 
-const StyledDiv = styled.div`
-  color: red;
+const StyledCard = styled.div`
+  margin: 0px auto;
+  border: solid blue 2px;
+  margin: 2%;
+  overflow-wrap: wrap;
+
+  .title {
+
+  }
+
+  .card-content{
+
+  }
 `;
 
-export default function Skill ({name, description, skillLevel}) {
+export default function Skill({e}) {
   return (
-    <StyledDiv>
-      {/* <h1>{name}</h1>
-      <div>{description}</div>
-      <div>{skillLevel}</div> */}
-    </StyledDiv>
+    <StyledCard className='card'>
+      <div className='title'>
+        <h1>{e.name}</h1>
+        <div className='card-content'>
+          <div>{e.desc}</div>
+          <div>{e.skillLevel}</div>
+        </div>
+      </div>
+    </StyledCard>
   );
 };
