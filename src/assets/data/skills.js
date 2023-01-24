fc89f6b789a545522ffab716ff3ae11d6ddde2e9
Joe Park <sunghun.josephp@gmail.com>
2023-01-23 18:31:59 -0800

    Add Real data to arr

--- src/assets/data/skills.js
+++ src/assets/data/skills.js
@@ -1,17 +1,32 @@
 module.exports = [
   {
-    'name': 'HTML',
-    'desc': 'this is a description',
+    'name': 'HTML & CSS',
+    'desc': 'The building blocks of all my websites.',
     'skillLevel': 5,
   },
   {
-    'name': 'CSS',
-    'desc': 'CSS is time consuming',
+    'name': 'JavaScript',
+    'desc': 'For both front and backend development',
     'skillLevel': 4,
   },
   {
-    'name': 'JavaScript',
-    'desc': 'JS is cool',
+    'name': 'Front-end frameworks',
+    'desc': 'React.js, Angular, Django',
     'skillLevel': 4,
+  },
+  {
+    'name': 'Back-end frameworks',
+    'desc': 'Node.js, Express',
+    'skillLevel': 4,
+  },
+  {
+    'name': 'Version Control Software',
+    'desc': 'Git and Git Hub',
+    'skillLevel': 4
+  },
+  {
+    'name': 'Cloud data platforms',
+    'desc': 'MongoDB Atlas, AWS',
+    'skillLevel': 3,
   },
 ];
